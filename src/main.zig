ac1026b13ea7bed9f15f54b348954d3a9537c45d
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2022-07-21 00:03:18 +0900

    const

--- src/main.zig
+++ src/main.zig
@@ -4,22 +4,22 @@ const fs = std.fs;
 fn cat(w: anytype, r: anytype) !void {
     var buf: [std.mem.page_size]u8 = undefined;
     while (true) {
-        var nread = try r.read(buf[0..]);
+        const nread = try r.read(buf[0..]);
         if (nread <= 0) break;
         try w.writeAll(buf[0..nread]);
     }
 }
 
 pub fn main() anyerror!void {
-    var a = std.heap.page_allocator;
+    const a = std.heap.page_allocator;
 
-    var args = try std.process.argsAlloc(a);
+    const args = try std.process.argsAlloc(a);
     defer a.free(args);
 
     const writer = std.io.getStdOut().writer();
     if (args.len > 1) {
         for (args[1..args.len]) |arg| {
-            var f = try fs.cwd().openFile(arg, fs.File.OpenFlags{ .mode = .read_only });
+            const f = try fs.cwd().openFile(arg, fs.File.OpenFlags{ .mode = .read_only });
             defer f.close();
             cat(writer, f) catch |err| {
                 std.log.warn("error reading file '{s}': {}\n", .{ arg, err });
